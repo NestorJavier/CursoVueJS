42a9a8d8132c803ba2d4f1cf516b09008856a4ec
Nestor Mendez <nestormendezg@outlook.com>
2022-10-22 20:01:17 -0500

    starting Vuejs3 course

--- #3 methosds, computed/app.js
+++ #3 methosds, computed/app.js
@@ -1,24 +1,25 @@
-let app = new Vue({//Instanciamos vue
-    el: '#app',//Identificamos el container con el que se ha de trabajar con vue mediante su id="app"
-    data:{//Data es una parte de vue, por lo tanto siempre debe llevar ese nombre
-        titulo: 'Hola Mundo Con Vue',
-        total: 0,
-        nuevaFruta: '',
-        frutas: [
-                    {nombre: "pera", cantidad: 10},
-                    {nombre: "platano", cantidad: 0},
-                    {nombre: "manzana", cantidad: 11}
-                ],
+let app = Vue.createApp({//Instanciamos vue
+    data: function () {//Data es una parte de vue, por lo tanto siempre debe llevar ese nombre
+        return {
+            titulo: 'Hola Mundo Con Vue',
+            total: 0,
+            nuevaFruta: '',
+            frutas: [
+                { nombre: "pera", cantidad: 10 },
+                { nombre: "platano", cantidad: 0 },
+                { nombre: "manzana", cantidad: 11 }
+            ]
+        }
     },
     methods: {//Palabra reservada methods
-        agregarFruta(){
+        agregarFruta() {
             this.frutas.push({
                 nombre: this.nuevaFruta, cantidad: 0
             })
         }
     },
     computed: {//Los computed son un arreglo
-        sumarFrutas(){
+        sumarFrutas() {
             this.total = 0;
             for (const fruta of this.frutas) {//Este loop se va a ejecutar cada vez que se detecte un cambio en la cantidad
                 this.total += fruta.cantidad;
@@ -27,3 +28,54 @@ let app = new Vue({//Instanciamos vue
         }
     },
 })
+app.component('login-form',
+        {
+            template:`
+                <form @submit.prevent="handleSubmit">
+                    <h1>{{ title }}<h1/>
+                    <custom-input v-model:label="emailLabel"/>
+                    <custom-input :label="passwordLabel"/>
+                    <button>Log In<button>
+                <form/>
+            `,
+            components:['custom-input'],
+            data(){
+                return {
+                    title: 'Login Form',
+                    email: '',
+                    password: '',
+                    emailLabel: 'Email',
+                    passwordLabel: 'Password'
+                }
+            },
+            methods:{
+                handleSubmit(){
+                    console.log(this.email, this.password)
+                }
+            }
+        }
+    )
+
+    app.component('custom-input',
+        {
+            template:`
+                <label>
+                    {{ label }}
+                    <input type="text" v-model="inputValue"/>
+                <label/>
+            `,
+            props:['label'],
+            computed:{
+                get(){
+
+                },
+                set(){
+
+                }
+            }
+            /* data(){
+                return{inputValue:''}
+            } */
+        }
+    )
+app.mount('#app')
